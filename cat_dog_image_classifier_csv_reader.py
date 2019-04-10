0cc362c94309a71c2e0faf6b40c0b4781f271e29
Andrew <arwest@uiowa.edu>
2019-04-10 13:46:31 -0500

    Create cat_dog_classifier_csv_reader.py
    - gets the associated expected outcome and adoption speed for each observation from data that has at least 1 image to be used with the cat/dog classifier
    - also creates a txt file of ids to in the same order as their np matrix info position

---
+++ cat_dog_image_classifier_csv_reader.py
@@ -0,0 +1,45 @@
+import csv
+import os
+import numpy as np
+from os.path import isfile, join
+
+type_index = 0
+adoption_speed_index = 23
+pet_id_index = 21
+
+def create_type_speed_array():
+    folder_dir = os.curdir + '/petfinder-adoption-prediction/train_images_npy'
+
+    ids = []
+    for file in os.listdir(folder_dir):
+        name = file.split('-')[0]
+        if isfile(join(folder_dir, file)) and not ids.__contains__(file):
+            ids.append(name)
+
+    dic = dict()
+    with open(join(os.curdir,'petfinder-adoption-prediction/train.csv')) as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=',')
+        line = 0
+        for row in csvReader:
+            if line == 0:
+                line += 1
+                continue
+            dic[row[pet_id_index]] = (row[type_index], row[adoption_speed_index])
+
+    results_dir = join(os.curdir, 'petfinder-adoption-prediction/train_images_results')
+    if not os.path.isdir(results_dir):
+        os.mkdir(results_dir)
+
+    data= []
+    with open(results_dir + '/ids.txt', 'w') as id_file:
+        for id in ids:
+            (animal_type, adoption_speed) = dic[id]
+            data.append([animal_type, adoption_speed])
+            id_file.write(str(id) + '\n')
+        data_matrix = np.array(data, dtype=np.int32)
+
+    np.save(join(os.curdir, 'petfinder-adoption-prediction/train_images_results/matrix'), data_matrix)
+
+    return data_matrix
+
+create_type_speed_array()
