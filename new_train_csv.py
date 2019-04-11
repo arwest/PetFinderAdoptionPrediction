20a304b4a2c30256e022fde8a02b0809db6a91de
Andrew <arwest@uiowa.edu>
2019-04-11 09:54:29 -0500

    bug fixes to work around having commas in description field of csv file

--- new_train_csv.py
+++ new_train_csv.py
@@ -4,7 +4,7 @@ import os
 from os.path import isfile, join
 
 desc_index = 20
-pet_id_index = 21
+pet_id_index = -3
 
 def create_new_train_csv():
     sent = dict()
@@ -25,14 +25,14 @@ def create_new_train_csv():
         line = 0
         for row in csvReader:
             if line == 0:
-                headers = row[:19] + ['DescMagnitude', 'DescScore'] + row[21:]
+                headers = row[:20] + ['DescMagnitude', 'DescScore'] + row[-3:]
                 line += 1
                 continue
             try:
                 (magnitude, score) = sent[row[pet_id_index]]
             except KeyError:
                 magnitude = score = 'NaN'
-            new_row_data = row[:19] + [magnitude, score] + row[21:]
+            new_row_data = row[:20] + [magnitude, score] + row[-3:]
             data.append(new_row_data)
 
     with open(join(os.curdir, 'petfinder-adoption-prediction', 'train_wo_desc.csv'), 'w', newline='') as csvFile:
