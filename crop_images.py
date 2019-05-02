57549cf0972b890ef9089075d6605a02fc2ce66d
Andrew <arwest@uiowa.edu>
2019-05-02 13:35:11 -0500

    cleaned up version

--- crop_images.py
+++ crop_images.py
@@ -10,7 +10,7 @@ results_dir = join(os.curdir, 'petfinder-adoption-prediction', 'train_crop_image
 if not os.path.isdir(results_dir):
     os.mkdir(results_dir)
 
-index = 0
+count = 0
 train_meta_list = os.listdir(train_meta_path)
 train_meta_list.sort()
 for file in train_meta_list:
@@ -32,25 +32,9 @@ for file in train_meta_list:
     with Image.open(join(train_img_path, file_name+'.jpg')) as img:
         crop_img = img.crop([vertex_pairs[0][0], vertex_pairs[0][1],
                              vertex_pairs[2][0] - 1, vertex_pairs[2][1] - 1])
-        crop_img.save(join(results_dir, file), 'JPEG')
+        crop_img.save(join(results_dir, file_name+'.jpg'), 'JPEG')
 
-    index += 1
-    if (index+1) % 5000 == 0:
-        print(index+1, 'images cropped')
+    count += 1
+    if (count) % 5000 == 0:
+        print(count, 'images cropped')
 print('All images cropped')
-#
-# index = 0
-# train_img_list = os.listdir(train_img_path)
-# train_img_list.sort()
-# for file in train_img_list:
-#     name = file.split('.')[0]
-#     if isfile(join(train_img_path, file)):
-#         img = Image.open(join(train_img_path, file))
-#         vertex_list = vertices[name]
-#         crop_img = img.crop([vertex_list[0][0], vertex_list[0][1],
-#                              vertex_list[2][0] - 1, vertex_list[2][1] - 1])
-#         crop_img.save(join(results_dir, file), 'JPEG')
-#         index += 1
-#         if (index+1) % 5000 == 0:
-#             print('Images saved for', index + 1, 'images')
-# print('Cropped images saved for all images')
